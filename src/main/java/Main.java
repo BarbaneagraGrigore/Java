66be3ba8019522406ae6338be800100f2b166e05
gbarbaneagra <grigorebarbaneagra@gmail.com>
2021-08-02 12:54:43 +0300

    Java challange

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,32 +8,31 @@ public class Main {
 
         System.out.println("Give an integer number: ");
         int number = scanner.nextInt();
-        System.out.println("Give another integer number: ");
-        int number2 = scanner.nextInt();
-
-        System.out.println("Give a text: ");
-        Scanner scanner1 = new Scanner(System.in);
-        String text = scanner1.nextLine();
-
-        System.out.println("Give a operation from +,-,/,* : ");
-        Scanner scanner3 = new Scanner(System.in);
-        char operator = scanner3.next().charAt(0);
-
-        scanner1.close();
+//        System.out.println("Give another integer number: ");
+//        int number2 = scanner.nextInt();
+//
+//        System.out.println("Give a text: ");
+//        scanner.nextLine();
+//        String text = scanner.nextLine();
+//
+//        System.out.println("Give a operation from +,-,/,* : ");
+//        char operator = scanner.next().charAt(0);
+//
         scanner.close();
-        scanner3.close();
+//
+//        PrintedText.printText(text);
+//        RemoverOfLetter.removeX(text);
+//        PrintedNumbers.printNumbers(number);
+//        MaximumOfArray.largest();
+//        RandomPopulatedArray.populateArray(number);
+//        RandomPopulatedDoubleArray.populateDoubleArray(number, number2);
+//        Comparator2Numbers.findMaxMin(number, number2);
+//        Calculator.calculate(number, number2, operator);
+//        Swiper.swipeNumbers(number, number2);
+//        PrintedNumbersWhileLoop.displayWithWhileLoop(number);
+//
+//        AverageOfArray.findAverageOfArray(number);
 
-        PrintedText.printText(text);
-        RemoverOfLetter.removeX(text);
-        PrintedNumbers.printNumbers(number);
-        MaximumOfArray.largest();
-        RandomPopulatedArray.populateArray(number);
-        RandomPopulatedDoubleArray.populateDoubleArray(number, number2);
-        Comparator2Numbers.findMaxMin(number,number2);
-        Calculator.calculate(number,number2,operator);
-        Swiper.swipeNumbers(number,number2);
-        PrintedNumbersWhileLoop.displayWithWhileLoop(number);
-
-        AverageOfArray.findAverageOfArray(number);
+//        DataTypes.typeDataTypes(number);
     }
 }

21fefe425cd23dab6931be419e684d5d87631904
gbarbaneagra <grigorebarbaneagra@gmail.com>
2022-01-22 15:32:16 +0200

    Reverse of String, when symbols and digits remain on the place

---
+++ src/main/java/java/challanges/ReverserOfString.java
@@ -0,0 +1,48 @@
+package java.challanges;
+
+import java.util.regex.Pattern;
+
+public class ReverserOfString {
+
+    public static void reverseString(String string) {
+        StringBuilder sb = new StringBuilder(string);
+        System.out.println(sb.reverse());
+    }
+
+    public static boolean stringHasDigits(String string) {
+        return string.matches(".*\\d.*");
+    }
+
+    public static boolean stringHasSymbols(String string) {
+        Pattern special = Pattern.compile("[!@#$%&*()_+=|<>?{}\\[\\]~-]");
+        return string.matches(String.valueOf(special));
+    }
+
+    public static void reverse(String string) {
+        if (stringHasDigits(string) || (stringHasSymbols(string))) {
+            System.out.println(reverseStringWithDigitsAndSymbols(string));
+        } else {
+            reverseString(string);
+        }
+    }
+
+    public static String reverseStringWithDigitsAndSymbols(String string) {
+        char[] stringToChar = string.toCharArray();
+        int startArray = 0;
+        int endArray = stringToChar.length - 1;
+        while (startArray < endArray) {
+            if (!Character.isAlphabetic(stringToChar[startArray])) {
+                startArray++;
+            } else if (!Character.isAlphabetic(stringToChar[endArray])) {
+                endArray--;
+            } else {
+                char temporary = stringToChar[startArray];
+                stringToChar[startArray] = stringToChar[endArray];
+                stringToChar[endArray] = temporary;
+                startArray++;
+                endArray--;
+            }
+        }
+        return new String(stringToChar);
+    }
+}
